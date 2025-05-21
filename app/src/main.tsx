ceea6ea26602151f9f5d9a1af2749e1314ed8538
Artur_Karakashyan <atupatu2005@mail.ru>
2025-05-21 18:36:26 +0300

    myau

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import  App  from './App.tsx';
-import './styles/index.css';
 import './styles/variables.scss';
 import './styles/normalize.scss';
-import './styles/class-names.scss';
 
 
 createRoot(document.getElementById('root')!).render(
